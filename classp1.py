28e393da37c0c6b1659fa915d0b3e826cc4f4703
MarcosUrbano1 <142849111+MarcosUrbano1@users.noreply.github.com>
2023-08-28 13:49:45 -0300

    criei o classp1

---
+++ classp1.py
@@ -0,0 +1,11 @@
+class Pessoa:
+
+    def __init__(self, nome, idade, altura, peso):
+        self.nome = nome
+        self.idade = idade
+        self.altura = altura
+        self.peso =  peso
+
+        pessoa1 = Pessoa (LilmarHellstar, 27, 1,79, 70)
+
+print (pessoa1.nome)
