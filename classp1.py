24697aa12f0d0d549cfee34a1b7e93127f1f7781
MarcosUrbano1 <142849111+MarcosUrbano1@users.noreply.github.com>
2023-09-10 22:09:09 +0000

    separei a classe do resto do comando por arquivos.

--- classp1.py
+++ classp1.py
@@ -1,5 +1,3 @@
-import re
-
 class Pessoa:
 
     def __init__(self, nome, idade, altura, peso, nascimento):
@@ -9,50 +7,23 @@ class Pessoa:
         self.peso =  peso
         self.nascimento = nascimento
 
-    def apresentar(self):
-        print(f'Muito prazer!Eu me chamo {self.nome} e tenho {self.idade} anos de idade.')
+    @property
+    def nome(self):
+        return self.nome
+    
+    @property
+    def idade(self):
+        return self.idade
+    
+    @property
+    def altura(self):
+        return self.altura
 
-    def eh_maior(self):
-        return self.idade >= 18
-    
-    def imc(self): 
-        return self.peso / (self.altura * self.altura)
-    
-    def imc_longo(self):
-        imc= self.imc()
-        if imc <18.5:
-            return 'Abaixo do peso.'
-        elif imc <25.0:
-            return 'Peso normal.'
-        elif imc <30.0:
-            return 'Excesso de peso.'
-        elif imc <35.0:
-            return 'Obesidade 1.'
-        elif imc <40.0:
-            return 'Obesidade 2.'
-        else:
-            return 'Obesidade 3.'
+    property
+    def peso(self):
+        return self.peso
 
-    def comparar_idade(self,idade):
-        if idade < self.idade:
-            print(f'{self.nome} é mais velho que eu!')
-        elif idade > self.idade:
-            print(f'{self.nome} é mais novo que eu!')
-        else:
-            print(f'{self.idade} tem a mesma idade que eu!')
-
-    def validar_nascimento(self,nascimento):
-        self.nascimento = r'^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$'
-        return re.match(self.nascimento, nascimento)
-    
-    def validar_nome(self,nome):
-        self.nome = r'^([A-Za-z])$'
-        return re.match(self.nome, nome)
-    
-    def validar_altura(self,altura):
-        self.altura = r'^([0-90-9].[0-9])$'
-        return re.match(self.altura, altura)
-    
-    def validar_peso(self,peso):
-        self.peso = r'^([0-90-9].[0-9])$'
-        return re.match(self.peso, peso)
+    @property
+    def nascimento(self):
+        data = f'{self.nascimento[3:5]}/{self.nascimento[0:2]}/{self.nascimento[6:10]}'
+        return data
